42f91848ff4c2afa06d3ae7bbc04f7cb983a59e7
Mohammed Al Meraikhi <130155541+almeraikhi@users.noreply.github.com>
2025-02-05 16:28:22 +0400

    fix(books): implemented base styling for the books list page

--- apps/app/src/features/books/components/BookItem.tsx
+++ apps/app/src/features/books/components/BookItem.tsx
@@ -3,13 +3,25 @@ import dayjs from 'dayjs';
 
 export const BookItem = ({ book }: { book: APIBook }) => {
   return (
-    <div>
-      <div className='text-lg'>{book.title}</div>
-      <div className='opacity-80'>{book.author.name}</div>
-      <div className='opacity-80'>{dayjs(book.publishedAt).format('YYYY')}</div>
-      <div className='opacity-80'>{book.ISBN}</div>
-      <div className='text-sm'>
-        {book.genres.map((genre) => genre.genre.name).join(', ')}
+    <div className='flex md:flex-row flex-col gap-2 items-center border border-gray-200 rounded-md p-4 md:justify-between'>
+      <div>
+        <img
+          src='/images/book-placeholder-image.jpg'
+          alt={book.title}
+          className='w-40 h-60 object-cover rounded-md'
+        />
+      </div>
+      <div>
+        <div className='text-xl text-center'>{book.title}</div>
+        <div className='opacity-80 text-center'>by {book.author.name}</div>
+      </div>
+      <div className='flex md:flex-row flex-col md:gap-2 items-center'>
+        <div className=''>{dayjs(book.publishedAt).format('YYYY')}</div>
+        <div>{book.genres.map((genre) => genre.genre.name).join(' • ')}</div>
+      </div>
+      <div className='flex flex-col items-center'>
+        <div className='opacity-80'>ISBN</div>
+        <div>{book.ISBN}</div>
       </div>
     </div>
   );
--- apps/app/src/features/books/components/BooksList.tsx
+++ apps/app/src/features/books/components/BooksList.tsx
@@ -8,6 +8,8 @@ import { z } from 'zod';
 import { GetAllBooksParams } from '@repo/prisma/transactions/books.transactions';
 import { useForm } from 'react-hook-form';
 import { useMemo } from 'react';
+import { Button } from '~/components/Button';
+import { useNavigate } from 'react-router-dom';
 
 // const GetBooksParamsSchema = z.object({
 //   authorId: z.string().optional(),
@@ -17,6 +19,12 @@ import { useMemo } from 'react';
 export const BooksList = () => {
   const { data: authors, isLoading: isLoadingAuthors } = useGetAuthors();
   const { data: genres, isLoading: isLoadingGenres } = useGetGenres();
+
+  const navigate = useNavigate();
+
+  const navigateToAddBook = () => {
+    navigate('/books/add');
+  };
 
   const form = useForm<GetAllBooksParams>({
     defaultValues: {
@@ -58,21 +66,42 @@ export const BooksList = () => {
   }
 
   return (
-    <div className='flex flex-col gap-2'>
-      <Select
-        onChange={(value) => {
-          if (!value) return;
-          form.setValue('authorId', value.value, { shouldValidate: true });
-        }}
-        value={selectedAuthor ?? null}
-        options={authors.map((author) => ({
-          label: author.name,
-          value: author.id,
-        }))}
-      />
-      {books.map((book) => (
-        <BookItem book={book} />
-      ))}
+    <div className='flex justify-center items-center h-full'>
+      <div className='flex flex-col gap-2 px-4 md:max-w-screen-lg w-full h-full'>
+        <Select
+          isClearable
+          onChange={(value) => {
+            if (!value) {
+              form.setValue('authorId', undefined, { shouldValidate: true });
+            } else {
+              form.setValue('authorId', value.value, { shouldValidate: true });
+            }
+          }}
+          value={selectedAuthor ?? null}
+          options={authors.map((author) => ({
+            label: author.name,
+            value: author.id,
+          }))}
+        />
+        <Select
+          isMulti
+          isClearable
+          onChange={(value) => {
+            if (!value) return;
+            const ids = value.map((genre) => genre.value);
+            form.setValue('genresIds', ids, { shouldValidate: true });
+          }}
+          value={selectedGenres ?? null}
+          options={genres.map((genre) => ({
+            label: genre.name,
+            value: genre.id,
+          }))}
+        />
+        <Button onClick={navigateToAddBook}>Add Book...</Button>
+        {books.map((book) => (
+          <BookItem book={book} />
+        ))}
+      </div>
     </div>
   );
 };
