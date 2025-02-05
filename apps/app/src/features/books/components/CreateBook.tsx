de1789dd9ce3890a1327e9e8db1a014b6fcedaaf
Mohammed Al Meraikhi <130155541+almeraikhi@users.noreply.github.com>
2025-02-05 13:44:35 +0400

    fix(books): fixed form to have its own input typing

--- apps/app/src/features/books/components/CreateBook.tsx
+++ apps/app/src/features/books/components/CreateBook.tsx
@@ -4,14 +4,24 @@ import { Input } from '~/components/Input';
 import { Select } from '~/components/Select';
 import { useGetAuthors } from '~/features/authors/api/getAuthors';
 
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+type CreateBookFormInput = Omit<CreateBookInput, 'authorId' | 'genresIds'> & {
+  author: SelectOption;
+  genres: SelectOption[];
+};
+
 export const CreateBook = () => {
   const { data: authors, isLoading: isLoadingAuthors } = useGetAuthors();
 
-  const form = useForm<CreateBookInput>({
+  const form = useForm<CreateBookFormInput>({
     defaultValues: {
       title: '',
-      authorId: '',
-      genresIds: [],
+      author: undefined,
+      genres: [],
       ISBN: '',
       publishedAt: new Date(),
     },
@@ -25,13 +35,18 @@ export const CreateBook = () => {
       <Input {...form.register('title')} placeholder='Title' />
       <Select
         placeholder='Author...'
+        onChange={(value) => {
+          if (!value) return;
+          form.setValue('author', value);
+        }}
+        value={form.watch().author}
         options={authors.map((author) => ({
           label: author.name,
           value: author.id,
         }))}
       />
-      <Input {...form.register('authorId')} placeholder='Author ID' />
-      <Input {...form.register('genresIds')} placeholder='Genres IDs' />
+      {/* <Input {...form.register('authorId')} placeholder='Author ID' /> */}
+      {/* <Input {...form.register('genresIds')} placeholder='Genres IDs' /> */}
       <Input {...form.register('ISBN')} placeholder='ISBN' />
       <Input {...form.register('publishedAt')} placeholder='Published At' />
     </div>
