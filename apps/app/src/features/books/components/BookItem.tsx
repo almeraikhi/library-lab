cd8694022e5880d9220e148f86669808b8edc12b
Mohammed Al Meraikhi <130155541+almeraikhi@users.noreply.github.com>
2025-02-06 11:42:18 +0400

    fix(books): styling of the listing

--- apps/app/src/features/books/components/BookItem.tsx
+++ apps/app/src/features/books/components/BookItem.tsx
@@ -12,21 +12,21 @@ export const BookItem = ({ book }: { book: APIBook }) => {
   return (
     <button
       onClick={navigateToBook}
-      className='flex md:flex-row flex-col gap-2 items-center border border-gray-200 rounded-md p-4 md:justify-between w-full'
+      className='grid md:grid-cols-4 grid-rows-1 gap-2 items-center justify-center border border-gray-200 rounded-md p-4 w-full'
     >
-      <div>
+      <div className='flex items-center justify-center'>
         <img
           src='/images/book-placeholder-image.jpg'
           alt={book.title}
           className='min-w-40 h-60 object-cover rounded-md'
         />
       </div>
-      <div>
+      <div className='flex flex-col gap-1 flex-shrink-0'>
         <div className='text-xl text-center'>{book.title}</div>
         <div className='opacity-80 text-center'>by {book.author.name}</div>
+        <div>Published at {dayjs(book.publishedAt).format('YYYY')}</div>
       </div>
-      <div className='flex md:flex-row flex-col md:gap-2 items-center'>
-        <div className=''>{dayjs(book.publishedAt).format('YYYY')}</div>
+      <div className='flex md:flex-row flex-col md:gap-2 items-center max-w-72'>
         <div>{book.genres.map((genre) => genre.genre.name).join(' • ')}</div>
       </div>
       <div className='flex flex-col items-center'>
--- apps/app/src/router/routes/book.route.tsx
+++ apps/app/src/router/routes/book.route.tsx
@@ -3,14 +3,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 import Breadcrumb from '~/components/Breadcumb/Breadcrumb';
 import { Button } from '~/components/Button';
 import { useBook } from '~/features/books/api/useBook';
-import { useBookLogs } from '~/features/books/api/useBookLogs';
-import { updatedDiff } from 'deep-object-diff';
 import { BookUpdateLogs } from '~/features/books/components/BookUpdateLogs';
 
 export const BookRoute = () => {
   const { id } = useParams();
   const { data, isLoading } = useBook({ id: id! });
-  const { data: logs, isLoading: isLoadingLogs } = useBookLogs({ id: id! });
   const navigate = useNavigate();
 
   const handleGoBack = () => {
@@ -38,7 +35,7 @@ export const BookRoute = () => {
     <div>
       <Breadcrumb items={breadcrumbs} />
       <div className='flex flex-col gap-4'>
-        <div className='flex md:flex-row flex-col items-center'>
+        <div className='flex md:flex-row flex-col items-center gap-1 md:justify-between'>
           <div>
             <img
               src='/images/book-placeholder-image.jpg'
@@ -46,17 +43,24 @@ export const BookRoute = () => {
               className='min-w-40 h-60 object-cover rounded-md'
             />
           </div>
-          <div className='flex flex-col items-center'>
+          <div className='flex flex-col items-center flex-shrink-0'>
             <div className='text-2xl'>{data.title}</div>
             <div className='opacity-80'>by {data.author.name}</div>
-            <div className='flex gap-2'>
-              <div>Genres</div>
-              {data.genres.map((genre) => genre.genre.name).join(', ')}
+            <div>Published at {dayjs(data.publishedAt).format('YYYY')}</div>
+          </div>
+          <div className='flex flex-col items-center'>
+            <div className='font-bold'>Genres</div>
+            <div className='text-center'>
+              {data.genres.map((genre) => genre.genre.name).join(' • ')}
             </div>
           </div>
-          <div>Published at {dayjs(data.publishedAt).format('YYYY')}</div>
-          <div>ISBN: {data.ISBN}</div>
-          <Button onClick={handleUpdateBook}>Update Book</Button>
+          <div className='flex flex-col items-center'>
+            <div className='font-bold'>ISBN</div>
+            <div>{data.ISBN}</div>
+          </div>
+          <Button className='max-w-fit' onClick={handleUpdateBook}>
+            Update Book
+          </Button>
         </div>
         <div>
           <div>Change Logs</div>
